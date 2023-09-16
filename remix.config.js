6b8d99485a4c9dcdc57cb51208671f5845eca0c6
Youxi Luo <immccn123@outlook.com>
2023-09-16 10:31:14 +0000

    migrate to remix v2

--- remix.config.js
+++ remix.config.js
@@ -7,12 +7,5 @@ module.exports = {
   // publicPath: "/build/",
   serverModuleFormat: "cjs",
   // serverDependenciesToBundle: ['axios'],
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
+  future: {},
 };
