ee1fc1800f1b4783ee1d68998cbf5ff6a8af9e35
Imken Luo <immccn123@outlook.com>
2023-09-21 19:18:38 +0800

    add robots.txt

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
     <>
       <h1>犇犇保存站</h1>
       <Segment>
-        <h2>Imken 的服务器娘一共发现了……</h2>
+        <h2>你站一共发现了</h2>
         <div>
           <Statistic>
             <Statistic.Value>{data.content.today}</Statistic.Value>
@@ -39,12 +39,14 @@ export default function Index() {
       <Segment>
         <h2>关于本站</h2>
         <p>使用 React + Semantic 编写的船新犇犇保存站前端壳子！</p>
-        <p>不对数据完整性和正确性负责 有一段时间的数据好像乱了自己看着办懒得处理了</p>
-        <p>还有，你站一直都是 Imken 一个人运营，而且免费对外提供 API 接口（https://api-lgf.imken.moe/docs），要用的自取</p>
+        <p>不对数据完整性和正确性负责 有一段时间的数据好像乱了自己看着办懒得处理了ww</p>
+        <p>现在新壳子基本上稳定了 以后应该不会大改了哦</p>
+        <p>还有，你站一直都是 Imken 一个人运营，而且对外提供 API 接口（https://api-lgf.imken.moe/docs），要用的自取，要开跨域的联系咱就行了哦</p>
+        <p>你站不叫 Imken 保存站！！不要拿这个指代你站！！</p>
         <p>咕咕咕</p>
         <p>—— By Imken</p>
         <p>
-          如果可以的话，<a href="https://sponsor.imken.moe">支持一下谢谢喵！</a>
+          如果可以的话，<a href="https://sponsor.imken.moe">投喂支持</a>一下谢谢喵！
         </p>
       </Segment>
     </>
