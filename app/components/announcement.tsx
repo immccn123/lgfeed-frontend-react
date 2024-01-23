999841541a450e0a2907d636bc1fa43d318d3260
Imken Luo <immccn123@outlook.com>
2024-01-23 14:15:13 +0800

    remove announcement

--- app/components/announcement.tsx
+++ app/components/announcement.tsx
@@ -1,6 +1,7 @@
 import { Message } from "semantic-ui-react";
 
 export const Announcement = () => {
+  return null;
   return (
     <Message info>
       若近期在境内访问本站较艰难，您可以使用 aws-benben.imken.dev 备用节点。
