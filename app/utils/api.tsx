4378bf4b8871c4c347e87f0a59ed82b5c8892053
Imken Luo <me@imken.moe>
2024-04-22 22:16:00 +0800

    upd new api

--- app/utils/api.tsx
+++ app/utils/api.tsx
@@ -1,6 +1,6 @@
 import axois from "axios";
 
-export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "https://api-benben.imken.dev";
 
 export const api = axois.create({
   baseURL: BASE_URL,
