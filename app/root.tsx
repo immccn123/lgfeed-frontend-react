e3585109489e520ce76dd5fbbec220261e1d9413
Imken Luo <immccn123@outlook.com>
2024-04-30 19:34:58 +0800

    service paused

--- app/root.tsx
+++ app/root.tsx
@@ -76,7 +76,7 @@ export default function App() {
         <MainMenu />
         <Container>
           <Announcement />
-          <Outlet />
+          {/* <Outlet /> */}
         </Container>
         <Footer arcEnv={env} />
         <ScrollRestoration />
