9e0bfd744d952ac706cb8cc80d6b9cff102d1c4e
Imken Luo <me@imken.moe>
2024-07-27 14:36:21 +0800

    fix vercel deploy

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,13 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { vercelPreset } from '@vercel/remix/vite';
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+    })
+  ],
   resolve: {
     alias: {
       "~": "/app",
