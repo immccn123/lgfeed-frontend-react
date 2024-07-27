ec7649e9ddced2926dced7685b113199a39fae3f
Imken Luo <me@imken.moe>
2024-07-27 14:52:00 +0800

    test

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,6 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { installGlobals } from '@remix-run/node';
 import { vercelPreset } from '@vercel/remix/vite';
+
+installGlobals();
 
 export default defineConfig({
   plugins: [
