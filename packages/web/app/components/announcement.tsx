117b6dd3a80bcb21a1fd029d9357fc4c16e2b1cd
Imken <me@imken.moe>
2024-09-17 21:07:43 +0800

    fix typo

--- packages/web/app/components/announcement.tsx
+++ packages/web/app/components/announcement.tsx
@@ -11,7 +11,7 @@ export const Announcement = () => {
         margin: "0",
       }}
     >
-      意见收集/问题返回请加 QQ 群：313404608 | 支持我们继续做下去！
+      意见或问题反馈请加 QQ 群：313404608 | 支持我们继续做下去！
       <a href="https://sponsor.imken.moe">捐赠（请备注为犇站）</a>
     </Message>
   );
